ab4a45bdf0994057aab15e948142bc2237f9e208
kc <151539784+33kc@users.noreply.github.com>
2025-01-11 19:08:24 +0530

    Create bestTimetoBuyAndSell.cpp

---
+++ leetcode/easy/bestTimetoBuyAndSell.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int l = 0, r = 1, mp = 0;
+        while (r < prices.size())
+        {
+            if (prices[r] < prices[l]) l = r;
+            else mp = max(mp, prices[r] - prices[l]);
+            r++;
+        }
+        return mp;
+    }
+};
