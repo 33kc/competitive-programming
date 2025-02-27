41a06044239464780727a3143b275ca396ed906d
kc <151539784+33kc@users.noreply.github.com>
2025-02-27 14:32:43 +0530

    Create base.cpp

---
+++ boilerplate/base.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// constants and macros that'll most likely be useful
+#define INF INT_MAX
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ALL(x) (x).begin(),(x).end()
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define TAKEINPUT(vec, n) for(int i = 0; i < (n); i++) cin>>vec[i];
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+
+void solve() 
+{
+    // main code
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    
+    int t = 0; cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+
+    return 0;
+}
